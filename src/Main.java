0272602047db96e0f1644851f8829d9ecfb8f0db
321ru <gerarr999@yandex.ru>
2022-11-29 16:58:16 +0300

    Kursach

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.sql.SQLOutput;
 import java.util.Random;
 
 public class Main {
-    private static final Employee[] EMPLOYEES = new Employee[10];
+    private static  Employee[] EMPLOYEES = new Employee[10];
 
     public static void main(String[] args) {
         EMPLOYEES[0] = new Employee("Муратов Руслан Мергенович", 1, 50_000);
@@ -14,11 +14,11 @@ public class Main {
         int totalMoney = getTotalMoney();
         getTotalMoney();
         System.out.println("Всего деняг " + totalMoney);
-         minSalary();
-        System.out.println("Минимальная зарплата у "+minSalary());
+        minSalary();
+        System.out.println("Минимальная зарплата у " + minSalary());
         maxSalary();
-        System.out.println("Максимальная зарплата у "+maxSalary());
-        System.out.println("Средняя зарплата "+midSalary());
+        System.out.println("Максимальная зарплата у " + maxSalary());
+        System.out.println("Средняя зарплата " + midSalary());
         printFullName();
     }
 
@@ -52,29 +52,32 @@ public class Main {
         }
         return employeeMinMoney;
     }
+
     private static Employee maxSalary() {
         int max = Integer.MIN_VALUE;
         Employee employeeMaxMoney = null;
         for (Employee employee : EMPLOYEES) {
-            if (employee != null && employee.getMoney() >max) {
+            if (employee != null && employee.getMoney() > max) {
                 max = employee.getMoney();
                 employeeMaxMoney = employee;
             }
         }
         return employeeMaxMoney;
     }
+
     private static double midSalary() {
-        int count =0;
-        for (Employee employee :EMPLOYEES){
+        int count = 0;
+        for (Employee employee : EMPLOYEES) {
             if (employee != null) {
                 count++;
             }
-            if (count != 0){
+            if (count != 0) {
                 return (double) getTotalMoney() / count;
             }
         }
-        return  0;
+        return 0;
     }
+
     private static void printFullName() {
         for (Employee employee : EMPLOYEES) {
             if (employee != null) {
